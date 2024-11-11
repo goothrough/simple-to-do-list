dafa86b1b2c4893d1dda1bf8e9c8349d3838720f
Suguru Ohashi <goothrough315@gmail.com>
2024-11-11 15:51:24 -0800

    add some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { TaskCardComponent } from './shared/task-card/task-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    TaskCardComponent,
   ],
   imports: [
     BrowserModule
